849cde2ad923c937bf85e131474f44dac04c54df
Can Batu Karakoyunlu <batucankarakoyunlu@gmail.com>
2026-08-13 21:51:49 +0300

    moved to std::any_of

--- Tinyfull-Logger/TinyfullLogger.hpp
+++ Tinyfull-Logger/TinyfullLogger.hpp
@@ -124,16 +124,17 @@ private:
 
     static void printHeader(FILE* stream, Level t, Category m) {
         bool color = TinyLoggerAnsiCheck::supportsANSI(stream);
-        bool all_empty = true;
         std::array styles{getLevel(t), getCategory(m)};
+        bool not_empty = std::any_of(styles.begin(), styles.end(), [](const auto& st){
+            return !st.name.empty();
+        });
 
         for (const auto& st : styles) {
             if (st.name.empty()) continue;
-            if (all_empty) all_empty = false;
             if (color) std::print(stream, "[{}{}\x1b[0m]", st.color, st.name);
             else std::print(stream, "[{}]", st.name);
         }
-        if (!all_empty) std::print(stream, " ");
+        if (not_empty) std::print(stream, " ");
     }
 public:
     template <typename... Args>
